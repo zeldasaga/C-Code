e891ab02c63e383ee0ab7cf3bf8107d90e480cab
zldaswrd <zeldasaga@gmail.com>
2016-09-12 16:00:06 -0600

    Add files via upload

---
+++ Create_Array.c
@@ -0,0 +1,8 @@
+/*
+ * Create an array of <length'> bytes long.  We will use an array of chars to store both chars and integers
+ */
+#include "Project0.h"
+char* Create_Array(int length) {
+    static char data[100];
+    return data;
+}
